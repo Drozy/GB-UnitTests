89ef9f1c00417f71a9227a3175290f821161d09f
Yakovlev Andrew <drozy@yandex.ru>
2023-11-02 17:59:52 +0300

    Tasks from fifth seminar added

--- src/main/java/seminars/five/number/RandomNumberModule.java
+++ src/main/java/seminars/five/number/RandomNumberModule.java
@@ -1,5 +1,17 @@
 package seminars.five.number;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RandomNumberModule {
 
+    public static List<Integer> getList(int count) {
+        Random rnd = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            list.add(rnd.nextInt(10));
+        }
+        return list;
+    }
 }
--- src/test/java/seminars/five/SeleniumTest.java
+++ src/test/java/seminars/five/SeleniumTest.java
@@ -1,6 +1,35 @@
 package seminars.five;
 
 
-public class SeleniumTest {
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SeleniumTest {
+    /**
+     * Напишите автоматизированный тест, который выполнит следующие шаги:
+     * 1. Открывает главную страницу Google.
+     * 2. Вводит "Selenium" в поисковую строку и нажимает кнопку "Поиск в Google".
+     * 3. В результатах поиска ищет ссылку на официальный сайт Selenium
+     * (https://www.selenium.dev) и проверяет, что ссылка действительно присутствует
+     * среди результатов поиска.
+     */
+    @Test
+    void seleniumTest() throws InterruptedException {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.google.com");
+        WebElement webElement = webDriver.findElement(By.name("q"));
+        webElement.sendKeys("Selenium");
+        webElement.submit();
+        WebElement seleniumEl = webDriver.findElement(By.tagName("cite"));
+
+        assertThat(seleniumEl.getText()).isEqualTo("https://www.selenium.dev");
+
+        Thread.sleep(3000);
+        webDriver.quit();
+    }
 }
